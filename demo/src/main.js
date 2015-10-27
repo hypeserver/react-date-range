f0066c8907b4d81a907946d9c3e768322c31ca10
Burak Can <neoberg@gmail.com>
2015-10-27 14:38:49 +0200

    v0.1.8

--- demo/src/main.js
+++
@@ -1,5 +0,0 @@
-import React from 'react';
-import ReactDom from 'react-dom';
-import Main from 'components/Main';
-
-ReactDom.render(<Main />, document.getElementById('root'));
--- src/index.js
+++
@@ -1,5 +0,0 @@
-import DateRange from './DateRange.js';
-import Calendar from './Calendar.js';
-import defaultRanges from './defaultRanges.js';
-
-export default { DateRange, Calendar, defaultRanges }
