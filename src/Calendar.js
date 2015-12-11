e9992cab6102361eaf09e85196d1575c0c00029f
mattfurness <matt.furness@gmail.com>
2015-12-11 11:48:04 +1000

    Change the previous month's days calculation to include the last day of the previous month

--- src/Calendar.js
+++ src/Calendar.js
@@ -150,7 +150,7 @@ class Calendar extends Component {
 
     // Previous month's days
     const diff = (Math.abs(firstDayOfWeek - (startOfMonth + 7)) % 7);
-    for (let i = diff; i >= 1; i--) {
+    for (let i = diff-1; i >= 0; i--) {
       const dayMoment  = lastMonth.clone().date(lastMonthDayCount - i);
       days.push({ dayMoment, isPassive : true });
     }
