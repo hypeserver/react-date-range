fb74ede6cfd37fc35ecf840883c13e9203d148b0
anderson06 <andersoncampos.m@gmail.com>
2016-10-13 17:34:23 -0300

    Fix dst day on firefox

--- src/utils/parseInput.js
+++ src/utils/parseInput.js
@@ -4,13 +4,13 @@ export default function parseInput(input, format) {
   let output = null;
 
   if (typeof input === 'undefined' ||  typeof input === 'null' || !input || input === '') {
-    output = moment().startOf('day');
+    output = moment().hours(12);
   } else if (typeof input === 'string') {
-    output = moment(input, format).startOf('day');
+    output = moment(input, format).hours(12);
   } else if (typeof input === 'function') {
-    output = parseInput( input(moment().startOf('day')) , format);
+    output = parseInput( input(moment().hours(12)) , format);
   } else if (input._isAMomentObject) {
-    output = input.startOf('day').clone();
+    output = input.hours(12).clone();
   }
 
   return output;
