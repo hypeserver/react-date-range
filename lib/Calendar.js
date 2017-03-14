hypeserver/react-date-range

commit 67bbc56fe76c5bffe87a7ab7bc6fc85eb2eb73c7

bug fix: update calendar shownDate

lib/Calendar.js
@@ -103,8 +103,9 @@ var Calendar = (function (_Component) {
     value: function componentWillReceiveProps(nextProps) {
       var range = nextProps.range;
       var offset = nextProps.offset;
+      var oldRange = this.props.range;
 
-      if (range && range['endDate'] && !range['endDate'].isSame(range['startDate'], 'day')) {
+      if ((range && range['endDate'] && !range['endDate'].isSame(range['startDate'], "day")) || (oldRange && !oldRange["startDate"].isSame(range["startDate"]))) {
         this.setState({ shownDate: range['endDate'].clone().add(offset, 'months') });
       }
     }

src/Calendar.js
@@ -60,8 +60,9 @@ class Calendar extends Component {
 
   componentWillReceiveProps(nextProps) {
     const { range, offset } = nextProps;
+    const oldRange = this.props.oldRange;
 
-    if(range && range['endDate'] && !range['endDate'].isSame(range['startDate'], 'day')) {
+    if ((range && range['endDate'] && !range['endDate'].isSame(range['startDate'], "day")) || (oldRange && !oldRange["startDate"].isSame(range["startDate"]))) {
       this.setState({ shownDate : range['endDate'].clone().add(offset, 'months') })
     }
   }

commit db46d5d294d824f84ef6541f640fd869ab539b93

Fix property name typo introduced in 67bbc56f

lib/Calendar.js
@@ -104,7 +104,7 @@ var Calendar = (function (_Component) {
       var range = nextProps.range;
       var offset = nextProps.offset;
 
-      var oldRange = this.props.oldRange;
+      var oldRange = this.props.range;
 
       if (range && range['endDate'] && !range['endDate'].isSame(range['startDate'], "day") || oldRange && !oldRange["startDate"].isSame(range["startDate"])) {
         this.setState({ shownDate: range['endDate'].clone().add(offset, 'months') });

src/Calendar.js
@@ -60,7 +60,7 @@ class Calendar extends Component {
 
   componentWillReceiveProps(nextProps) {
     const { range, offset } = nextProps;
-    const oldRange = this.props.oldRange;
+    const oldRange = this.props.range;
 
     if ((range && range['endDate'] && !range['endDate'].isSame(range['startDate'], "day")) || (oldRange && !oldRange["startDate"].isSame(range["startDate"]))) {
       this.setState({ shownDate : range['endDate'].clone().add(offset, 'months') })