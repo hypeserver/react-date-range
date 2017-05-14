6326a1c396f3bfabec740aae067ec894b6234963
Or Kazaz <or.kazaz@autodesk.com>
2017-05-14 10:11:44 +0300

    Updated PropTypes usage to remove react warings for 15.5.x + remove the lib folder from the repo

--- src/PredefinedRanges.js
+++ src/PredefinedRanges.js
@@ -1,4 +1,5 @@
-import React, { Component, PropTypes } from 'react';
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 import moment from 'moment';
 import classnames from 'classnames';
 import parseInput from './utils/parseInput.js';
