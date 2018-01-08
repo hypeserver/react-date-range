a3f7311d0ad6099adee5c734cc47234826001445
mkg0 <mehmetkamilgulen@gmail.com>
2018-01-08 14:49:15 +0300

    restructure demo and dependencies

---
+++ demo/src/main.dev.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { AppContainer } from 'react-hot-loader';
+import Main from './components/Main';
+
+const render = Component => {
+  ReactDOM.render(
+    <AppContainer>
+      <Main />
+    </AppContainer>,
+    document.getElementById('root'),
+  )
+}
+
+render(Main)
+
+// Webpack Hot Module Replacement API
+if (module.hot) {
+  module.hot.accept('./components/Main', () => {
+    render(Main)
+  })
+}
