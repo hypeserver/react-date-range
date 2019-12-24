80a3d7aa61de3302397c94168762ed646a22681c
Onur Kerimov <onurkerimov@gmail.com>
2019-12-24 15:52:16 +0300

    Modify PostCSS configuration

--- postcss.config.js
+++ postcss.config.js
@@ -1,7 +1,5 @@
-const precss = require('precss');
-const autoprefixer = require('autoprefixer');
-const cssImport = require('postcss-import');
-
 module.exports = {
-  plugins: [cssImport(), precss(), autoprefixer()],
-};
+  plugins: {
+    autoprefixer: {}
+  }
+}
