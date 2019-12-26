f1bd1074f7fdfe4726be54a67a0f5fc64482c328
Onur Kerimov <onurkerimov@gmail.com>
2019-12-26 15:58:58 +0300

    Add enzyme-to-json serializer

--- jest.config.js
+++ jest.config.js
@@ -2,4 +2,5 @@ module.exports = {
   verbose: true,
   setupFiles: ['<rootDir>/setupTests.js'],
   testPathIgnorePatterns: ['/node_modules/', '/dist/', '/demo/dist/'],
+  snapshotSerializers: ['enzyme-to-json/serializer'],
 };
