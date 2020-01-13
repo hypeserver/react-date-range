c7a3a1751020179541266b820bf31016e76370f1
Onur Kerimov <onurkerimov@gmail.com>
2020-01-13 11:50:21 +0300

    build gh-pages branch

--- src/index.js
+++
@@ -1,5 +0,0 @@
-export { default as DateRange } from './components/DateRange';
-export { default as Calendar } from './components/Calendar';
-export { default as DateRangePicker } from './components/DateRangePicker';
-export { default as DefinedRange } from './components/DefinedRange';
-export { defaultInputRanges, defaultStaticRanges, createStaticRanges } from './defaultRanges';
